e5da1e3d0ee11fa62ee0da1fbe71569a309c7c78
Christian Döring <christian.doering@tum.de>
2023-08-26 11:39:23 +0200

    Basic Buffer copy operations

--- candle-core/examples/wgpu_basics.rs
+++ candle-core/examples/wgpu_basics.rs
@@ -2,6 +2,11 @@ use anyhow::Result;
 use candle_core::{Device, Tensor};
 
 fn main() -> Result<()> {
-    let device = Device::new_wgpu(0)?;
+    let vulkan = Device::new_wgpu(0)?;
+
+    let x = Tensor::from_slice(&[1f32, 2., 3., 4.], (2, 2), &vulkan)?;
+
+    dbg!(x.to_vec2::<f32>()?);
+
     Ok(())
 }
--- candle-core/src/wgpu_backend.rs
+++ candle-core/src/wgpu_backend.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code)]
-use std::sync::Arc;
+use std::ops::Deref;
+use std::sync::{Arc, Mutex, MutexGuard};
 
+use futures::executor::block_on;
+use wgpu::util::DeviceExt;
+
+use crate::backend::BackendStorage;
 use crate::op::{BinaryOpT, CmpOp, ReduceOp, UnaryOpT};
 use crate::{CpuStorage, DType, Layout, Result, Shape};
 
@@ -14,34 +19,160 @@ impl From<WgpuError> for crate::Error {
 }
 
 #[derive(Debug)]
-struct Device {}
+pub struct Device {
+    instance: wgpu::Instance,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+impl Deref for Device {
+    type Target = wgpu::Device;
+
+    fn deref(&self) -> &Self::Target {
+        &self.device
+    }
+}
+impl Device {
+    fn create() -> Self {
+        block_on(async {
+            let instance = wgpu::Instance::default();
+
+            let adapter = instance
+                .request_adapter(&wgpu::RequestAdapterOptions::default())
+                .await?;
+
+            let (device, queue) = adapter
+                .request_device(
+                    &wgpu::DeviceDescriptor {
+                        label: None,
+                        features: wgpu::Features::empty(),
+                        limits: wgpu::Limits::downlevel_defaults(),
+                    },
+                    None,
+                )
+                .await
+                .unwrap();
+
+            let encoder =
+                device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+            Some(Self {
+                instance,
+                adapter,
+                device,
+                queue,
+            })
+        })
+        .unwrap()
+    }
+}
 
 #[derive(Debug)]
-struct Buffer {}
+pub struct Buffer {
+    dtype: DType,
+    device: WgpuDevice,
+    buffer: wgpu::Buffer,
+}
+impl Buffer {
+    fn size(&self) -> usize {
+        self.buffer.size() as _
+    }
+    fn uninit(device: &WgpuDevice, size: usize, dtype: DType) -> Self {
+        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: size as _,
+            usage: wgpu::BufferUsages::COPY_DST
+                | wgpu::BufferUsages::COPY_SRC
+                | wgpu::BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+
+        Buffer {
+            dtype: dtype,
+            device: device.clone(),
+            buffer,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct WgpuDevice(Arc<Device>);
+impl Deref for WgpuDevice {
+    type Target = Arc<Device>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 #[derive(Debug)]
 pub struct WgpuStorage(Arc<Buffer>);
+impl Deref for WgpuStorage {
+    type Target = Arc<Buffer>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 impl crate::backend::BackendStorage for WgpuStorage {
     type Device = WgpuDevice;
 
-    fn try_clone(&self, _: &Layout) -> Result<Self> {
-        todo!()
+    fn try_clone(&self, layout: &Layout) -> Result<Self> {
+        let dst = Buffer::uninit(&self.device, self.size(), self.dtype());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        encoder.copy_buffer_to_buffer(&self.buffer, 0, &dst.buffer, 0, self.buffer.size());
+        self.device.queue.submit(Some(encoder.finish()));
+        Ok(WgpuStorage(Arc::new(dst)))
     }
 
     fn dtype(&self) -> DType {
-        todo!()
+        self.dtype
     }
 
     fn device(&self) -> &Self::Device {
-        todo!()
+        &self.device
     }
 
     fn to_cpu_storage(&self) -> Result<CpuStorage> {
-        todo!()
+        let staging_buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: self.buffer.size(),
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        encoder.copy_buffer_to_buffer(&self.buffer, 0, &staging_buffer, 0, self.buffer.size());
+
+        self.device.queue.submit(Some(encoder.finish()));
+
+        let (sender, mut receiver) = futures::channel::oneshot::channel();
+
+        let buffer_slice = staging_buffer.slice(..);
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+        self.device.poll(wgpu::MaintainBase::Wait);
+
+        block_on(async {
+            receiver.try_recv().unwrap();
+            let slice = buffer_slice.get_mapped_range();
+            match self.dtype() {
+                DType::U8 => Ok(CpuStorage::U8(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::U32 => Ok(CpuStorage::U32(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::I64 => Ok(CpuStorage::I64(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::BF16 => Ok(CpuStorage::BF16(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::F16 => Ok(CpuStorage::F16(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::F32 => Ok(CpuStorage::F32(Vec::from(bytemuck::cast_slice(&slice)))),
+                DType::F64 => Ok(CpuStorage::F64(Vec::from(bytemuck::cast_slice(&slice)))),
+            }
+        })
     }
 
     fn affine(&self, _: &Layout, _: f64, _: f64) -> Result<Self> {
@@ -157,7 +288,7 @@ impl crate::backend::BackendStorage for WgpuStorage {
 impl crate::backend::BackendDevice for WgpuDevice {
     type Storage = WgpuStorage;
     fn new(_: usize) -> Result<Self> {
-        todo!()
+        Ok(Self(Arc::new(Device::create())))
     }
 
     fn location(&self) -> crate::DeviceLocation {
@@ -176,8 +307,30 @@ impl crate::backend::BackendDevice for WgpuDevice {
         todo!()
     }
 
-    fn storage_from_cpu_storage(&self, _: &CpuStorage) -> Result<Self::Storage> {
-        todo!()
+    fn storage_from_cpu_storage(&self, storage: &CpuStorage) -> Result<Self::Storage> {
+        let slice: &[u8] = match storage {
+            CpuStorage::U8(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::U32(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::I64(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::BF16(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::F16(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::F32(storage) => bytemuck::cast_slice(storage),
+            CpuStorage::F64(storage) => bytemuck::cast_slice(storage),
+        };
+
+        let buffer = self.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: None,
+            contents: slice,
+            usage: wgpu::BufferUsages::STORAGE
+                | wgpu::BufferUsages::COPY_DST
+                | wgpu::BufferUsages::COPY_SRC,
+        });
+
+        Ok(WgpuStorage(Arc::new(Buffer {
+            dtype: storage.dtype(),
+            device: self.clone(),
+            buffer,
+        })))
     }
 
     fn rand_uniform(&self, _: &Shape, _: DType, _: f64, _: f64) -> Result<Self::Storage> {
